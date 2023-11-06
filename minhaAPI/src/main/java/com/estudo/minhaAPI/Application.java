2c75ca2ddc8564901fee2607ce09f8c57b561e3c
jamcarvalho <jamcarvalho@gmail.com>
2023-11-06 11:23:29 +0000

    BugFix 06/11/2023 - v2.1

--- minhaAPI/src/main/java/com/estudo/minhaAPI/Application.java
+++ minhaAPI/src/main/java/com/estudo/minhaAPI/Application.java
@@ -1,8 +1,13 @@
 package com.estudo.minhaAPI;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description="Default URL")})
 @SpringBootApplication
 public class Application {
 
